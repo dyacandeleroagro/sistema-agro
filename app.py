946209681769aa9102cebd24aadb3bb949f6e01f
dyacandeleroagro <dyacandelero.agro@gmail.com>
2026-07-03 15:30:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ def get_conn():
 def check_password(usuario, password):
     conn = get_conn()
     cur = conn.cursor()
-cur.execute("SELECT nombre, rol FROM usuarios WHERE usuario = %s AND password = %s",(usuario, password))
+cur.execute("SELECT nombre, rol FROM usuarios WHERE usuario = %s AND password = %s",(usuario, password)
+)
     result = cur.fetchone()
     cur.close()
     if result:
