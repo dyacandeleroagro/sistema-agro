413f0ecafc659ee608e4fb470be2349732d5946d
dyacandeleroagro <dyacandelero.agro@gmail.com>
2026-07-31 20:22:44 +0000

    p26

--- app.py
+++ app.py
@@ -785,7 +785,7 @@ if menu == "🛡 SEGUROS Y COBERTURAS":
     if rol_actual == "Operario":
         st.error("No tiene permisos.")
         st.stop()
-        st.header("🛡 Control de Pólizas y Seguros")
+    st.header("🛡 Control de Pólizas y Seguros")
         with st.form("form_seguros", clear_on_submit=True):
             s1, s2, s3 = st.columns(3)
             with s1:
