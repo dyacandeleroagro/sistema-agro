160ad9a466b565d931a9047a791faf40cd39cf07
dyacandeleroagro <dyacandelero.agro@gmail.com>
2026-09-20 01:50:25 +0000

    st16

--- app.py
+++ app.py
@@ -1778,12 +1778,12 @@ if menu == "👥 SISTEMA DE TRIPULACIÓN":
                     # ==========================================
 
                     porcentaje_bonificacion = st.number_input(
-                     "🎁 Bonificación / Descuento (%)",
-                       min_value=-100.0,
-                       max_value=100.0,
-                       step=1.0,
-                       value=0.0,
-                       key="porcentaje_bonificacion_liquidacion"
+                        "🎁 Bonificación (%)",
+                        min_value=0.0,
+                        max_value=100.0,
+                        step=1.0,
+                        value=0.0,
+                        key="porcentaje_bonificacion_liquidacion"
                     )
 
                     monto_bonificacion = (
@@ -1798,7 +1798,7 @@ if menu == "👥 SISTEMA DE TRIPULACIÓN":
 
                     porcentaje_descuento = st.number_input(
                         "➖ Descuento (%)",
-                        min_value=0.0,
+                        min_value=-100.0,
                         max_value=100.0,
                         step=1.0,
                         value=0.0,
