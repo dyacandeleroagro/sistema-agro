8cf2035efc8629754b2cb4ec32c72caedf40efc5
dyacandeleroagro <dyacandelero.agro@gmail.com>
2026-09-20 19:22:54 +0000

    f8

--- app.py
+++ app.py
@@ -1671,7 +1671,7 @@ if menu == "👥 SISTEMA DE TRIPULACIÓN":
 
                         porcentaje_descuento = st.number_input(
                             "Descuento (%)",
-                            min_value=0.0,
+                            min_value=-100.0,
                             max_value=100.0,
                             step=1.0,
                             value=0.0,
@@ -1681,7 +1681,7 @@ if menu == "👥 SISTEMA DE TRIPULACIÓN":
                         monto_descuento = (
                             monto_base_trabajado
                             * porcentaje_descuento
-                            / 100
+                            / -100
                         )
 
                         st.write(
