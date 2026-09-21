0f03c7a1ac2f5b18906293b65cb570a694cc4947
dyacandeleroagro <dyacandelero.agro@gmail.com>
2026-09-21 19:57:11 +0000

    nts7

--- app.py
+++ app.py
@@ -2295,7 +2295,7 @@ if menu == "👥 SISTEMA DE TRIPULACIÓN":
                             "✅ Pago por hectárea guardado correctamente."
                         )
 
-                                                if nombres_comprobantes_hectareas:
+                        if nombres_comprobantes_hectareas:
 
                             for nombre_comprobante_hectareas in (
                                 nombres_comprobantes_hectareas
